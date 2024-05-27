192dda4e7c311c60b21521dc1957496406974e99
Abdallah Hamdy <37842966+xAbdalla@users.noreply.github.com>
2024-05-27 05:12:44 +0300

    Clear all Temp Files

---
+++ Batch Scripts/Clear_all_Temp_Files.bat
@@ -0,0 +1,44 @@
+@ECHO off
+Title Clear Temp Files Script By: Abdallah NouR ...
+:: BatchGotAdmin
+:: -------------------------------------
+REM  --> Check for permissions
+IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+	>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+	>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+IF '%errorlevel%' NEQ '0' (
+	ECHO Requesting administrative privileges...
+	GOTO UACPrompt
+) ELSE ( GOTO gotAdmin )
+
+:UACPrompt
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+SET params= %*
+ECHO UAC.ShellExecute "wt.exe", "%~s0", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+"%temp%\getadmin.vbs"
+DEL "%temp%\getadmin.vbs"
+EXIT /B
+
+:gotAdmin
+PUSHD "%CD%"
+CD /D "%~dp0"
+:: --------------------------------------  
+Title Clear Temp Files Script By: Abdallah NouR ...
+: START
+ECHO. & ECHO Clear Temp Files Script By: Abdallah NouR ... & ECHO. & ECHO.
+
+DEL /F/S/Q "%Temp%" > nul
+rmdir /S /Q "%Temp%" > nul
+
+REM schtasks.exe /Run /TN "\Microsoft\Windows\Servicing\StartComponentCleanup"
+REM DISM.exe /Online /Cleanup-Image /AnalyzeComponentStore
+
+
+ECHO Done ....
+PAUSE
+GOTO :eof
